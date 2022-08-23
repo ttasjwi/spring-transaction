b06209e0cbc2b74ea957111d19063cfd55f9cb83
ttasjwi <ttasjwi920@gmail.com>
2022-08-23 23:28:15 +0900

    Initial commit

---
+++ src/test/java/com/ttasjwi/springtx/SpringtxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttasjwi.springtx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
