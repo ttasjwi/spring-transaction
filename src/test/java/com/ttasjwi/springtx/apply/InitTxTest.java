2a047a032f8bbf78606454bf7b39844f75a9e420
ttasjwi <ttasjwi920@gmail.com>
2022-08-25 18:15:33 +0900

    트랜잭션 AOP 주의 사항 - 초기화 시점
    - `@PostConstructor`과 `@Transactional`을 함께 적용할 수 없다.
    - 대안 : `ApplicationReadyEvent` 이벤트를 사용

---
+++ src/test/java/com/ttasjwi/springtx/apply/InitTxTest.java
@@ -0,0 +1,53 @@
+package com.ttasjwi.springtx.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootTest
+public class InitTxTest {
+
+    @Autowired
+    Hello hello;
+
+    @Test
+    public void go() {
+        // 초기화 코드는 스프링이 초기화 시점에 호출한다.
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig {
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello {
+
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruct tx active = {}", txActive);
+        }
+
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
+        public void initV2() {
+            boolean txActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init ApplicationReady tx active = {}", txActive);
+        }
+    }
+}
