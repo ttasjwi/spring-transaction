fde329f64214fac365637be40ca532fb7447e9ce
ttasjwi <ttasjwi920@gmail.com>
2022-08-27 11:16:14 +0900

    내부트랜잭션 커밋, 외부 트랜잭션 커밋

--- src/test/java/com/ttasjwi/springtx/propagation/BasicTxTest.java
+++ src/test/java/com/ttasjwi/springtx/propagation/BasicTxTest.java
@@ -1,6 +1,7 @@
 package com.ttasjwi.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -74,5 +75,21 @@ public class BasicTxTest {
         TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션2 롤백");
         txManager.rollback(tx2);
+    }
+    
+    @Test
+    public void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction() = {}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction() = {}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
     }
 }
