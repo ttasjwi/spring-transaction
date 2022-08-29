2a4baeb535d0f40d8e30bf8d925f468e49a27bbe
ttasjwi <ttasjwi920@gmail.com>
2022-08-29 19:16:37 +0900

    모든 계층에 트랜잭션 적용(REQUIRED) - 예외를 잡아서 복구했음에도 롤백

--- src/test/java/com/ttasjwi/springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/ttasjwi/springtx/propagation/MemberServiceTest.java
@@ -5,7 +5,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -108,6 +110,25 @@ class MemberServiceTest {
         //when
         assertThatThrownBy(() -> memberService.joinV1(username))
                 .isInstanceOf(RuntimeException.class);
+
+        //then : 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional:ON Recover
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    @Test
+    public void recoverException_fail() {
+        //given
+        String username = "로그예외_recoverException_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username))
+                .isInstanceOf(UnexpectedRollbackException.class);
 
         //then : 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
