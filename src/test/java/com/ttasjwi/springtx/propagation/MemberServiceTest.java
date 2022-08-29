eecd37f2381765a291ef28c20e17a9009b450da4
ttasjwi <ttasjwi920@gmail.com>
2022-08-29 14:45:36 +0900

    서비스 트랜잭션 없이 두 독립된 트랜잭션이 커밋, 롤백

--- src/test/java/com/ttasjwi/springtx/propagation/MemberServiceTest.java
+++ src/test/java/com/ttasjwi/springtx/propagation/MemberServiceTest.java
@@ -1,10 +1,12 @@
 package com.ttasjwi.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
@@ -37,5 +39,24 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
 
+    }
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON
+     */
+    @Test
+    public void outerTxOff_fail() {
+        //given
+        String username = "로그예외";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then : 회원은 등록되는데 로그는 등록되지 않는다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
     }
 }
